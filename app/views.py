f276043db734e9f780ee65edd15e360f153fdb15
FireSpirit171 <rhodyrev7@gmail.com>
2024-04-04 05:51:04 +0300

    Обработка запросов

--- app/views.py
+++ app/views.py
@@ -1,28 +1,36 @@
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
 
 QUESTIONS = [
     {
+        "id": i,
         "title" : f"Question {i}",
         "text": f" This is question number {i}"
-    } for i in range(10)
+    } for i in range(100)
 ]
 
 ANSWERS = [
     {
         "text": f"Yes, I agree with you {i}",
-        "question_id": i%10
-    } for i in range(100)
+        "question_id": i%100
+    } for i in range(1000)
 ]
 
 def index( request ):
-    return render( request, "index.html", {"questions": QUESTIONS})
+    page_num = request.GET.get( 'page', 1 )
+    paginator = Paginator( QUESTIONS, 5 )
+    page_obj = paginator.page( page_num )
+    return render( request, "index.html", {"questions": page_obj})
 
 def hot( request ):
     questions = QUESTIONS[::-1]
-    return render( request, "hot.html", {"questions": questions})
+    page_num = request.GET.get( 'page', 1 )
+    paginator = Paginator( questions, 5 )
+    page_obj = paginator.page( page_num )
+    return render( request, "hot.html", {"questions": page_obj})
 
 def tag( request ):
     questions = QUESTIONS[4:7]
